e822e7c829cff206d5e8ffc6e986ed1ded51f500
gokmax <zenglingzheng@gmail.com>
2013-04-11 15:33:10 +0800

    final again...

---
+++ cs3Presentation/js/jquery.jplayer.settings.js
@@ -0,0 +1,35 @@
+<!--
+$(function(){
+	var playItem = 0,
+		title=$('.jp-interface .jp-title'),
+		jPlayer=$("#jplayer"),
+		myPlayList = [
+			{name:"Black Plant",mp3:"audio/1.mp3",ogg:"audio/black_plant.ogg"},
+			{name:"Hidden",mp3:"audio/2.mp3",ogg:"audio/hidden.ogg"},
+		],
+		jPlay=function(idx){
+			if(typeof idx==typeof 0)
+				jPlayer.jPlayer("setMedia",myPlayList[idx]).jPlayer('play')
+			if(typeof idx==typeof '')
+				jPlayer.jPlayer("setMedia",myPlayList[playItem=idx=='next'?(++playItem<myPlayList.length?playItem:0):(--playItem>=0?playItem:myPlayList.length-1)]).jPlayer('play')
+			title.text(myPlayList[playItem].name)
+			Cufon.refresh()
+		}
+
+	jPlayer.jPlayer({
+		ready: function() {
+			jPlay(playItem)
+		},
+		ended:function(){
+			jPlay('next')
+		}
+	})
+
+	$(".jp-prev,.jp-next")
+		.click( function() {
+			jPlay($(this).is('.jp-next')?'next':'prev')
+			return false;
+		})
+
+});
+-->
